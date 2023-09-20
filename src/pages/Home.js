e6505fff9def57f55023d2c858ac6598e7826562
Oyawe <oyawegoodness@gmail.com>
2023-09-20 00:32:01 -0700

    products section completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
   // console.log(filteredProducts)
   return <section className='py-16'>
     <div className="container mx-auto">
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0'>
         {filteredProducts.map(product => {
           return <Product product={product} key={product.id} />
         })}
