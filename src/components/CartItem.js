d59069eb23b02a05c289365e80edc2f8b9abd210
Oyawe <oyawegoodness@gmail.com>
2023-09-20 20:15:23 -0700

    cart items increment and decrement

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 //import link
 import { Link } from 'react-router-dom';
 //import icons
 import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io'
+// import cart context
+import { CartContext } from '../contexts/CartContext';
 
 const CartItem = ({ item }) => {
+  const { removeFromCart, increaseAmount, decreaseAmount } = useContext(CartContext)
   //deconstructuring the item
   const { id, title, image, price, amount } = item;
-  return <div className='flex'>
+  return <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
     <div className='w-full min-h-[150px] flex items-center gap-x-4'>
       {/* image  */}
       <Link to={`/product/${id}`}>
@@ -19,26 +22,26 @@ const CartItem = ({ item }) => {
           {/* title  */}
           <Link to={`/product/${id}`} className='text-sm uppercase font-medium max-w[240px] text-primary hover:underline'>{title}</Link>
           {/* remove icon  */}
-          <div className='text-xl cursor-pointer'><IoMdClose className='text-gray-500 hover:text-red-500 transition' /></div>
+          <div onClick={() => removeFromCart(id)} className='text-xl cursor-pointer'><IoMdClose className='text-gray-500 hover:text-red-500 transition' /></div>
         </div>
         <div className=' flex gap-x-2 h-[36px] text-sm'>
           {/* quantity  */}
           <div className='flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium'>
             {/* minus icon  */}
-            <div className='flex-1 flex justify-center items-center cursor-pointer'>
+            <div onClick={() => decreaseAmount(id)} className='flex-1 flex justify-center items-center cursor-pointer'>
               <IoMdRemove />
             </div>
             {/* amount  */}
             <div className='h-full flex justify-center items-center px-2'>{amount}</div>
             {/* plus icon  */}
-            <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+            <div onClick={() => increaseAmount(id)} className='flex-1 h-full flex justify-center items-center cursor-pointer'>
               <IoMdAdd />
             </div>
           </div>
           {/* item price  */}
-          <div>$ {price}</div>
+          <div className='flex-1 flex justify-around items-center'>$ {price}</div>
           {/* final price  */}
-          <div className=''>{`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
+          <div className='flex-1 flex justify-end items-center text-primary font-medium'>{`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
         </div>
       </div>
     </div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,6 +5,9 @@ export const CartContext = createContext();
 const CartProvider = ({ children }) => {
   //cart state
   const [cart, setCart] = useState([]);
+  // item amount state
+  const [itemAmount, setItemAmount] = useState(0);
+
   //add to cart
   const addToCart = (product, id) => {
     const newItem = { ...product, amount: 1 }
@@ -28,9 +31,55 @@ const CartProvider = ({ children }) => {
     // console.log(product)
     // console.log(`${product.title} added to cart`);
   }
+
+  //remove from cart
+  const removeFromCart = (id) => {
+    const newCart = cart.filter((item) => {
+      return item.id !== id
+    })
+    setCart(newCart)
+  }
+
+  //clear cart
+  const clearCart = () => {
+    setCart([])
+  }
+
+  // increase amount
+  const increaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    // console.log(item)
+    addToCart(cartItem, id)
+    // console.log(`item ${id} added to cart`)
+  }
+
+  // decrease amount
+  const decreaseAmount = (id) => {
+    const cartItem = cart.find((item) => item.id === id);
+    // console.log(item)
+    if (cartItem) {
+      const newCart = cart.map((item) => {
+        if (item.id === id) {
+          return {
+            ...item,
+            amount: cartItem.amount - 1
+          }
+        } else {
+          return item;
+        }
+      })
+      setCart(newCart)
+    }
+    if (cartItem.amount < 2) {
+      removeFromCart(id)
+    }
+    // addToCart(item, id)
+    // console.log(`item ${id} added to cart`)
+  }
+
   // To display the products and amounts in the cart
   // console.log(cart)
-  return <CartContext.Provider value={{ cart, addToCart }}>
+  return <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, increaseAmount, decreaseAmount }}>
     {children}
   </CartContext.Provider>;
 };
