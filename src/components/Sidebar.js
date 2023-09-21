48e0185bca4f9608129cb2c03fb5bcf4184eded6
Oyawe <oyawegoodness@gmail.com>
2023-09-21 11:39:45 -0700

    productdetails section completed

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,12 +13,12 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext)
-  const { cart, clearCart, totalPrice } = useContext(CartContext)
+  const { cart, clearCart, totalPrice, itemAmount } = useContext(CartContext)
   // console.log(useContext(CartContext));
 
   return <div className={` ${isOpen ? 'right-0' : '-right-full'} w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px] overflow-scroll`}>
     <div className='flex items-center justify-between py-6 border-b'>
-      <div className='uppercase text-sm font-semibold'>Shopping Bag (0)</div>
+      <div className='uppercase text-sm font-semibold'>Shopping Bag ({itemAmount})</div>
       {/* icon  */}
       <div onClick={handleClose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
         <IoMdArrowForward className='text-2xl' />
