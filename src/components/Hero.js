570b8072d85080a8f6f5fe2120671b00a9eb9b1f
Oyawe <oyawegoodness@gmail.com>
2023-10-15 22:08:24 -0700

    authenticity

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,15 @@
 import React from 'react';
 //import images
-import WomanImg from '../img/woman_hero.png'
 //import Link
 import { Link } from 'react-router-dom';
 
 
 const Hero = () => {
   return <section className='h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24'>
-    <div className="container mx-auto flex justify-around h-full">
+    <div className="container flex items-center justify-center h-full mx-auto">
       {/* text  */}
       <div className='flex flex-col justify-center'>
-        <div className='font-semibold flex items-center uppercase'>
+        <div className='flex items-center font-semibold uppercase'>
           <div className='w-10 h-[2px] bg-black mr-3'></div>New Trend
         </div>
         <h1 className='text-[70px] leading-[1.1] font-light
@@ -18,13 +17,13 @@ const Hero = () => {
           PREMIUM SALES BONAZA <br />
           <span className='font-semibold'>MEN & WOMEN</span>
         </h1>
-        <Link to={`/`} className='self-start uppercase font-semibold border-b-2 border-primary'>
+        <Link to={`/`} className='self-start font-semibold uppercase border-b-2 border-primary'>
           Discover More
         </Link>
       </div>
       {/* image  */}
       <div className='hidden lg:block'>
-        <img src={WomanImg} alt="" />
+        {/* <img src={WomanImg} alt="" /> */}
       </div>
     </div>
   </section>;
